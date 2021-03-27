de6d27347294f68e12cc1c20645573922f7547ca
souravsingh09 <37875735+souravsingh09@users.noreply.github.com>
2021-03-27 15:38:25 +0530

    Update med_scrap.py

--- actor/spiders/med_scrap.py
+++ actor/spiders/med_scrap.py
@@ -15,59 +15,44 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+
         print("Start")
-        next_page = response.css('li.PagedList-skipToNext a').attrib['href']
-        print("Start 1")
-        if next_page is not None:
-            print("Start 2")
-            for post in response.css('h5.customLink.item-title'):
-                print("Start 3")
-                full_url = response.urljoin('https://academic.oup.com' + post.css('a').attrib['href'])
-                print("Start 4")
-                try:
-                    print("Start 5 ")
-                    yield scrapy.Request(full_url, callback=self.get_details)
-                    print("Start 6")
-                    yield response.follow(next_page,callback=self.parse)
-                    print("Start 7")
-                except :
-                    print("lol")
+        for post in response.css('h5.customLink.item-title'):
+
+            full_url = response.urljoin('https://academic.oup.com' + post.css('a').attrib['href'])
+            yield scrapy.Request(full_url, callback=self.get_details)
+
+            next_page = response.css('li.PagedList-skipToNext a').attrib['href']
+
+            print(next_page)
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
 
 
     def get_details(self, response):
             # This method is called on by the 'parse' method above. It scrapes the URLs
             # that have been extracted in the previous step.
-        # try:
-        #title_detail = response.css("section.abstract p.chapter-para::text").extract_first()
-        #print("Found details: " + title_detail )
-        #
-            #yield{
-            try :
-                print("Start 9")
-                result = {
-                    'url': response.url,
-                    'Introduction': response.css("div.title::text").get(),
-                    'Introduction_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text").get()),
 
-                    'Aim': response.css("div.title::text")[1].get(),
-                    'Aim_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[1].get()),
+            result = {
+                'url': response.url,
+                'Introduction': response.css("div.title::text").get(),
+                'Introduction_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text").get()),
 
-                    'Methods': response.css("div.title::text")[2].get(),
-                    'Methods_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[2].get()),
+                'Aim': response.css("div.title::text")[1].get(),
+                'Aim_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[1].get()),
 
-                    'Result': response.css("div.title::text")[3].get(),
-                    'Result_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[3].get()),
+                'Methods': response.css("div.title::text")[2].get(),
+                'Methods_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[2].get()),
 
-                    'Conclusion': response.css("div.title::text")[4].get(),
-                    'Conclusion_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[4].get())
+                'Result': response.css("div.title::text")[3].get(),
+                'Result_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[3].get()),
 
-                }
-                print("Start 10")
-                yield result
-            except:
-                print("out of syl")
+                'Conclusion': response.css("div.title::text")[4].get(),
+                'Conclusion_text': unidecode.unidecode(response.css("section.abstract p.chapter-para::text")[4].get())
 
+            }
 
+            yield result
 
             #Apify.pushData(result)
             #apify.pushData(output)
